56b08c78564c276a5aacaa936beb046a1a180d09
Will Scott <wscott1541@hotmail.com>
2020-06-18 20:51:27 +0100

    - A reformatted historic/plots.py adapted for new script names

---
+++ plots.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 18:52:03 2020
+
+@author: WS
+"""
+
+from time import time
+
+from datetime import datetime
+
+today = time()
+
+today_dt = datetime.fromtimestamp(today)
+today_string = datetime.strftime(today_dt,'%Y-%m-%d')
+
+print(today_string)
+
+year = round(float(datetime.strftime(today_dt,'%Y')))
+month = round(float(datetime.strftime(today_dt,'%m')))
+day = round(float(datetime.strftime(today_dt,'%d')))
+
+"""
+Plot graphs, starting from today
+"""
+
+import matplotlib.pyplot as plt
+import primary_user_functions as puf#includes importing the data, but not updating
+
+puf.plot_month_and_previous_distances(month,year,'Running')
+plt.show()
+puf.plot_month_and_previous_durations(month,year,'Running')
+plt.show()
+puf.plot_month_and_previous_distances(month,year,'Cycling')
+plt.show()
+puf.plot_month_and_previous_durations(month,year,'Cycling')
+plt.show()
+puf.plot_month_and_previous_distances(month,year,'Walking')
+plt.show()
+puf.plot_month_and_previous_durations(month,year,'Walking')
+plt.show()
+puf.plot_durations_all_previous(month,year,'Running')
+plt.show()
+puf.plot_distances_all_previous(month,year,'Running')
+plt.show()
+puf.plot_cumulative_distance(month,year,'Running')
+plt.show()
+puf.plot_cumulative_distance(month,year,'All')
+plt.show()
+puf.plot_week_and_previous_distances(today_string,'Running')
+plt.show()
+puf.plot_week_and_previous_distances(today_string,'Cycling')
+plt.show()
+puf.plot_week_and_previous_distances(today_string,'All')
+plt.show()
+
+import primary_user_functions as muf
+
+muf.plot_distances_this_month(month,year,'Running')
