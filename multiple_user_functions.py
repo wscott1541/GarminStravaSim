fc4805ab7e3bc03dca2a9562c75a7f4984be1eee
Will Scott <wscott1541@hotmail.com>
2020-06-19 19:26:26 +0100

    - Created function to plot distances over the past week

--- multiple_user_functions.py
+++ multiple_user_functions.py
@@ -29,8 +29,7 @@ day = round(float(datetime.strftime(today_dt,'%d')))
 
 import data_read as dr
 
-from primary_user_functions import month_length, date_string
-
+from primary_user_functions import month_length, date_string, populate_arrays_generic
 
 def populate_arrays(m,yyyy,month_dates,curr_vals):
     lim = month_length(m,yyyy)
@@ -71,14 +70,72 @@ def distance_sum(m,yyyy,activity,initials):#can add 'All'
     #error message?
     return(month_dates,sum_distances)
     
-def plot_distances_this_month(m,yyyy,activity):
+def plot_month_distances(m,yyyy,activity):
+    
+    title = '{} activities this month'.format(activity)
+    
+    if activity == 'All':
+        activity = 'i'#i being, of course, included in the suffix -ing as well as in Cardio
+    
     fig,ax = plt.subplots()
     for n in range(0,len(dr.initials_list)):
         
         month_dates, sum_distances = distance_sum(m,yyyy,activity,dr.initials_list[n])
-        annot = '{}'.format(dr.initials_list[n])
+        annot = '{}: {}km'.format(dr.initials_list[n],sum_distances[-1])
         
         plt.plot(month_dates, sum_distances,label = annot)
         
     ax.legend();
+    
+    plt.title(title)
+    
+def distance_sum_week(today_string,activity,initials):
+    date_strp = datetime.strptime(today_string,'%Y-%m-%d')
+    date_object = datetime.timestamp(date_strp)
+    
+    sum_distances = [0]
+    week_dates = [0]
+    
+    if activity == 'All':
+        activity = 'i'#i being, of course, included in the suffix -ing as well as in Cardio
+        
+    dates,distances,durations,types = dr.data_read(initials)
+        
+    for i in range(1,8):
+        temp_date_object = date_object - (24 * 60 * 60) * ((7-i))
+        temp_date_dt = datetime.fromtimestamp(temp_date_object)
+        temp_date_string = datetime.strftime(temp_date_dt,'%Y-%m-%d')
+        
+        for d in range (0,len(dates)):
+            if temp_date_string in dates[d] and activity in types[d]:
+                dist = round(sum_distances[-1] + float(distances[d]),2)
+                sum_distances.append(dist)
+                week_dates.append(i)
+   
+    populate_arrays_generic(7,week_dates,sum_distances)
+    
+    if week_dates[1] == 0:
+        sum_distances.remove(sum_distances[0])
+        week_dates.remove(week_dates[0])
+    
+    #error message?
+    return(week_dates,sum_distances)
+  
+def plot_distances_this_week(today_string,activity):
+   
+    title = '{} activities this week'.format(activity)
+    
+    fig,ax = plt.subplots()
+    for n in range(0,len(dr.initials_list)):
+        
+        month_dates, sum_distances = distance_sum_week(today_string,activity,dr.initials_list[n])
+        annot = '{}: {}km'.format(dr.initials_list[n],sum_distances[-1])
+        
+        plt.plot(month_dates, sum_distances,label = annot)
+        
+    ax.legend();
+   
+    plt.title(title)
+    
+
     
