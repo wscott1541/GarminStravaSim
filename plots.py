5deedd594bcd916bb85fbecfba6985cc8ae91f99
Will Scott <wscott1541@hotmail.com>
2020-06-21 18:20:19 +0100

    - Updated with today_string

--- plots.py
+++ plots.py
@@ -6,60 +6,43 @@ Created on Thu Jun 18 18:52:03 2020
 @author: WS
 """
 
-from time import time
-
-from datetime import datetime
-
-today = time()
-
-today_dt = datetime.fromtimestamp(today)
-today_string = datetime.strftime(today_dt,'%Y-%m-%d')
-
-print(today_string)
-
-year = round(float(datetime.strftime(today_dt,'%Y')))
-month = round(float(datetime.strftime(today_dt,'%m')))
-day = round(float(datetime.strftime(today_dt,'%d')))
+from today_string import today_string, year, month
 
 """
 Plot graphs, starting from today
 """
-
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import primary_user_functions as puf#includes importing the data, but not updating
+import multiple_user_functions as muf
+
+"""List plots"""
 
 
 puf.plot_month_distance(month,year,'Running')
-plt.show()
 
 puf.plot_month_and_previous_distances(month,year,'Running')
-plt.show()
+
 puf.plot_month_and_previous_durations(month,year,'Running')
-plt.show()
+
 puf.plot_month_and_previous_distances(month,year,'Cycling')
-plt.show()
+
 puf.plot_month_and_previous_distances(month,year,'Walking')
-plt.show()
 
 puf.plot_durations_all_previous(month,year,'Running')
-plt.show()
+
 puf.plot_distances_all_previous(month,year,'Running')
-plt.show()
+
 puf.plot_cumulative_distance(month,year,'Running')
-plt.show()
 
 puf.plot_cumulative_distance(month,year,'All')
-plt.show()
-puf.plot_week_and_previous_distances(today_string,'Running')
-plt.show()
-puf.plot_week_and_previous_distances(today_string,'Cycling')
-plt.show()
-puf.plot_week_and_previous_distances(today_string,'All')
-plt.show()
 
-import multiple_user_functions as muf
+puf.plot_week_and_previous_distances(today_string,'Running')
+
+puf.plot_week_and_previous_distances(today_string,'Cycling')
+
+puf.plot_week_and_previous_distances(today_string,'All')
 
 muf.plot_month_distances(month,year,'Running')
-plt.show()
+
 muf.plot_distances_this_week(today_string,'Running')
-plt.show()
+
