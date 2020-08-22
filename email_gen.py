988ae3b6a5d3b08f2ffa829ef0af646d26e35154
wscott1541 <37776322+wscott1541@users.noreply.github.com>
2020-08-22 12:51:27 +0100

    - Added update.py incorporation

--- email_gen.py
+++ email_gen.py
@@ -43,69 +43,90 @@ receiver_email = users_list[0]
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from today_string import day, month, year, today_string
+from today_string import day, month, year, today_string, y_day_string
 
-subject = "Snappily-named exercise update email (SNEUE), {} {} {}".format(day,month_caller(month),year)
-body = "Maybe one day this will also provide text insights. "
+subject = "SNEUE, {} {} {}".format(day,month_caller(month),year)
 
 message = MIMEMultipart()
 message["From"] = sender_email
 message["To"] = receiver_email
 message["Subject"] = subject
 
-message.attach(MIMEText(body, "plain"))
+#intro = "Maybe one day this will also provide text insights. "
+#message.attach(MIMEText(intro, "plain"))
 
-def attach_chart_as_html():
+body = puf.week_summary_html()
+#message.attach(MIMEText(intro,'html'))
+
+def attach_chart_as_html(body):
     plt.savefig('temp_image.jpg')
     
     encoded = base64.b64encode(open('temp_image.jpg','rb').read()).decode()
     
-    html = f"""\
-<html>
+    img = f"""\
  <body>
    <img src='data:image/jpg;base64,{encoded}'>
  </body>
-</html>
 """
     
-    part = MIMEText(html, "html")
-    message.attach(part)
+    new = body + img
+
+    #part = MIMEText(html, "html")
+    #message.attach(part)
     
     os.remove('temp_image.jpg')
     
+    return(new)
+    
 
 """List functions to send"""
+
+puf.plot_week_and_previous_distances(y_day_string,'Running')
+body = attach_chart_as_html(body)  
+puf.plot_week_and_previous_distances(y_day_string,'Cycling')
+body = attach_chart_as_html(body) 
+puf.plot_week_and_previous_distances(y_day_string,'All')
+body = attach_chart_as_html(body) 
 puf.plot_month_distance(month,year,'Running')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_month_and_previous_distances(month,year,'Running')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_month_and_previous_durations(month,year,'Running')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_month_and_previous_distances(month,year,'Cycling')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_month_and_previous_distances(month,year,'Walking')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_distances_this_year(month,year,'Running')
-attach_chart_as_html()
-puf.plot_durations_all_previous(month,year,'Running')
-attach_chart_as_html()
-puf.plot_distances_all_previous(month,year,'Running')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_cumulative_distance(month,year,'Running')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
 puf.plot_cumulative_distance(month,year,'All')
-attach_chart_as_html()
-puf.plot_week_and_previous_distances(today_string,'Running')
-attach_chart_as_html()  
-puf.plot_week_and_previous_distances(today_string,'Cycling')
-attach_chart_as_html() 
-puf.plot_week_and_previous_distances(today_string,'All')
-attach_chart_as_html()      
+body = attach_chart_as_html(body)
+puf.plot_durations_all_previous(month,year,'Running')
+body = attach_chart_as_html(body)
+puf.plot_distances_all_previous(month,year,'Running')
+body = attach_chart_as_html(body)
+
+     
 
 muf.plot_month_distances(month,year,'Running')
-attach_chart_as_html()
-muf.plot_distances_this_week(today_string,'Running')
-attach_chart_as_html()
+body = attach_chart_as_html(body)
+muf.plot_distances_this_week(y_day_string,'Running')
+body = attach_chart_as_html(body)
+
+outtro = puf.all_personal_bests_html()
+#message.attach(MIMEText(outtro, "html"))
+
+final = body + outtro
+
+html = f"""
+<html>
+{final}
+</html>
+"""
+
+message.attach(MIMEText(final,'html'))
 
 """Complete and send email"""
 
@@ -119,7 +140,6 @@ with smtplib.SMTP(smtp_server, port) as server:
     server.sendmail(sender_email, receiver_email, text)
         
 print('Sent to {}'.format(receiver_email))
-
 
    
 
